fd7251012a1f939f8ae02c3d207816b5958dce61
ruslanalekseyev <ruslan.alekseyev@gmail.com>
2018-01-01 19:10:02 +0200

    user get info

---
+++ app/api/controllers/AuthController.js
@@ -0,0 +1,55 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var passport = require('passport');
+
+module.exports = {
+  index: function(req, res) {
+    res.view();
+  },
+
+  logout: function(req, res) {
+    req.logout();
+    res.redirect('/');
+  },
+
+  // https://developers.facebook.com/docs/
+  // https://developers.facebook.com/docs/reference/login/
+  facebook: function(req, res) {
+    passport.authenticate('facebook', { failureRedirect: '/login', scope: ['email'] }, function(err, user) {
+      req.logIn(user, function(err) {
+        if (err) {
+          console.log(err);
+          res.view('500');
+          return;
+        }
+
+        res.redirect('/');
+        return;
+      });
+    })(req, res);
+  },
+
+  // https://developers.google.com/
+  // https://developers.google.com/accounts/docs/OAuth2Login#scope-param
+  google: function(req, res) {
+    passport.authenticate('google', { failureRedirect: '/login', scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read'] }, function(err, user) {
+      req.logIn(user, function(err) {
+        if (err) {
+          console.log(err);
+          res.view('500');
+          return;
+        }
+
+        res.redirect('/');
+        return;
+      });
+    })(req, res);
+  },
+
+};
+
